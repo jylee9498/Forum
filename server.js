272cc742f84d3fb5b62e376325c2eed545e4c1ce
이종율 <ijong-yul@ijong-yul-ui-MacBookPro.local>
2024-05-13 23:46:13 +0900

    add html file, bootstrap source

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express()
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(8080, () => {
-    console.log("http://localhost:8080 에서 서버 실행중")
+    console.log("http://localhost:8080 에서 서버 실행중");
 });
 
 app.get("/", (req,res)=>{
-    res.send("반갑습니다.");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/news", (req,res) =>{
+    res.send("뉴스입니다.");
+});
+
+app.get("/shop", (req,res) =>{
+    res.send("쇼핑페이지 입니다.");
+});
+
+app.get("/about", (req,res) =>{
+    res.sendFile(__dirname+"/mypage.html");
 });
