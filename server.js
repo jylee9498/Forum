f62bfbaa25c78726de04d7234565c76ec4adeb1f
이종율 <ijong-yul@ijong-yul-ui-MacBookPro.local>
2024-05-14 22:41:33 +0900

    Linked with MongoDB and implemented sending data to server

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const app = express()
+const app = express();
+
+const { MongoClient } = require('mongodb');
+
+ let db;
+ const url = "mongodb+srv://admin:1111@cluster0.u7x3pu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공');
+  db = client.db('Cluster0');
+  app.listen(8080, () => {
+    console.log("http://localhost:8080 에서 서버 실행중");
+});
+
+}).catch((err)=>{
+  console.log(err);
+});
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(8080, () => {
-    console.log("http://localhost:8080 에서 서버 실행중");
-});
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/news", (req,res) =>{
-    res.send("뉴스입니다.");
+    db.collection("post").insertOne({title : "어쩌구"});
 });
 
 app.get("/shop", (req,res) =>{
